f1745b63442d71f9b822bfffeec07a59378fff44
Abdurahim Jauzee <jauzee@ya.ru>
2017-02-09 16:51:55 +0300

    created ElongationAppearance object which can be used for tuning control configuration

---
+++ ElongationPreview/ElongationPreview/Source/ElongationAppearance.swift
@@ -0,0 +1,22 @@
+//
+//  ElongationAppearance.swift
+//  ElongationPreview
+//
+//  Created by Abdurahim Jauzee on 09/02/2017.
+//  Copyright © 2017 Ramotion. All rights reserved.
+//
+
+import UIKit
+
+
+public struct ElongationAppearance {
+  
+  public static var defaultAppearance = ElongationAppearance()
+  
+  public var frontViewHeight: CGFloat = 190
+  public var frontViewScaleFactor: CGFloat = 0.9
+  
+  public var backViewOffset: CGFloat = 28
+  public var backViewHeight: CGFloat = 170
+  
+}
