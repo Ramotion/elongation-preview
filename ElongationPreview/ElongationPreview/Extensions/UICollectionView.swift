f85ee42fa7ca5fe054af3004702978ef32a6d399
Abdurahim Jauzee <jauzee@ya.ru>
2017-02-09 16:51:21 +0300

    declared some useful UIKit extensions

--- ElongationPreview/ElongationPreview/Extensions/UICollectionView.swift
+++ ElongationPreview/ElongationPreview/Extensions/UICollectionView.swift
@@ -9,18 +9,18 @@
 import UIKit
 
 
-extension UICollectionView {
+public extension UICollectionView {
   
-  func register<T: UICollectionViewCell>(_: T.Type) {
+  public func register<T: UICollectionViewCell>(_: T.Type) {
     register(T.self, forCellWithReuseIdentifier: String(describing: T.self))
   }
   
-  func registerNib<T: UICollectionViewCell>(_:T.Type) {
+  public func registerNib<T: UICollectionViewCell>(_:T.Type) {
     let nib = UINib(nibName: String(describing: T.self), bundle: nil)
     register(nib, forCellWithReuseIdentifier: String(describing: T.self))
   }
   
-  func dequeue<T: UICollectionViewCell>(_: T.Type, indexPath: IndexPath) -> T {
+  public func dequeue<T: UICollectionViewCell>(_: T.Type, indexPath: IndexPath) -> T {
     return dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as! T
   }
   
--- ElongationPreview/ElongationPreview/Extensions/UITableView.swift
+++ ElongationPreview/ElongationPreview/Extensions/UITableView.swift
@@ -10,36 +10,28 @@
 import UIKit
 
 
-protocol TableViewConfigurable: UITableViewDelegate, UITableViewDataSource { }
-
-extension UITableView {
+public extension UITableView {
   
-  func setup(_ vc: TableViewConfigurable) {
-    delegate = vc
-    dataSource = vc
-  }
-  
-  func register<T: UITableViewCell>(_: T.Type) {
+  public func register<T: UITableViewCell>(_: T.Type) {
     register(T.self, forCellReuseIdentifier: String(describing: T.self))
   }
   
-  func registerNib<T: UITableViewCell>(_:T.Type) {
+  public func registerNib<T: UITableViewCell>(_:T.Type) {
     let nib = UINib(nibName: String(describing: T.self), bundle: nil)
     register(nib, forCellReuseIdentifier: String(describing: T.self))
   }
   
-  func dequeue<T: UITableViewCell>(_: T.Type) -> T {
+  public func dequeue<T: UITableViewCell>(_: T.Type) -> T {
     return dequeueReusableCell(withIdentifier: String(describing: T.self)) as? T ?? T()
   }
   
-  
   // MARK: Header & Footer
-  func registerNibHeader<T: UIView>(_: T.Type) {
+  public func registerNibHeader<T: UIView>(_: T.Type) {
     let nib = UINib(nibName: String(describing: T.self), bundle: nil)
     register(nib, forHeaderFooterViewReuseIdentifier: String(describing: T.self))
   }
   
-  func dequeueHeaderFooter<T: UIView>(_: T.Type) -> T {
+  public func dequeueHeaderFooter<T: UIView>(_: T.Type) -> T {
     return dequeueReusableHeaderFooterView(withIdentifier: String(describing: T.self)) as! T
   }
   
