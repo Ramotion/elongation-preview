ed2f4271693901f4e0a44597f8cf6ebbf227befe
Abdurahim Jauzee <jauzee@ya.ru>
2017-02-15 18:35:40 +0300

    updated `ElongationCell`
    - adopted `Expandable` protocol
    - added `encoding/decoding` implementation
    - renamed some views
    - changed animations

--- ElongationPreview/ElongationPreview/Source/Views/ElongationCell.swift
+++ ElongationPreview/ElongationPreview/Source/Views/ElongationCell.swift
@@ -9,24 +9,36 @@
 import UIKit
 
 
-open class ElongationCell: UITableViewCell {
+open class ElongationCell: UITableViewCell, Expandable {
   
   // MARK: Public properties
   open var isExpanded = false
   
-  @IBOutlet public var frontView: UIView!
-  @IBOutlet public var frontViewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet public var topView: UIView!
+  @IBOutlet public var topViewHeightConstraint: NSLayoutConstraint!
   
-  @IBOutlet public var backView: UIView!
-  @IBOutlet public var backViewHeightConstraint: NSLayoutConstraint!
-  @IBOutlet public var backViewTopConstraint: NSLayoutConstraint!
+  /// This is the top view which can be scaled if `scaleFactor` was configured in `ElongationAppearance`.
+  /// Also to this view can be applied 'parallax' effect.
+  @IBOutlet public var scalableView: UIView!
+  @IBOutlet public var scalableViewTopConstraint: NSLayoutConstraint!
+  @IBOutlet public var scalableViewBottomConstraint: NSLayoutConstraint!
+  
+  @IBOutlet public var bottomView: UIView!
+  @IBOutlet public var bottomViewHeightConstraint: NSLayoutConstraint!
+  @IBOutlet public var bottomViewTopConstraint: NSLayoutConstraint!
+  @IBOutlet public var bottomViewBottomConstraint: NSLayoutConstraint!
   
   // MARK: Private properties
-  fileprivate var dimmingView = UIView()
-  
-  fileprivate var appearance: ElongationAppearance {
-    return ElongationAppearance.defaultAppearance
+  fileprivate var dimmingView: UIView!
+  fileprivate var topSeparatorLine: UIView?
+  fileprivate var bottomSeparatorLine: UIView?
+  fileprivate var appearance: ElongationConfig {
+    return ElongationConfig.shared
   }
+  
+  fileprivate var scalableViewTopOffset: CGFloat!
+  fileprivate var scalableViewBottomOffset: CGFloat!
+  fileprivate var scalableViewLastParrallaxOffset: CGFloat!
   
   // MARK: Constructor
   public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -36,32 +48,26 @@ open class ElongationCell: UITableViewCell {
   
   public required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+    decode(from: aDecoder)
     commonInit()
   }
   
   private func commonInit() {
-    selectionStyle = .none
-    selectedBackgroundView = nil
-    clipsToBounds = true
-    contentView.clipsToBounds = true
-    
-    contentView.addSubview(dimmingView)
-    dimmingView.alpha = 0
-    dimmingView.backgroundColor = UIColor.black
-    dimmingView.frame = bounds
-    dimmingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    configureCell()
+    addDimmingView()
+    addCustomSeparatorIfNeeded()
   }
   
   open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-    let frontViewContainsPoint = frontView.frame.contains(point)
-    let backViewContainsPoint = backView.frame.contains(point)
+    let scalableViewContainsPoint = scalableView.frame.contains(point)
+    let backViewContainsPoint = bottomView.frame.contains(point)
     
-    if frontViewContainsPoint {
-      return frontView
+    if scalableViewContainsPoint {
+      return scalableView
     }
     
     if backViewContainsPoint {
-      return backView
+      return bottomView
     }
     
     return nil
@@ -72,6 +78,16 @@ open class ElongationCell: UITableViewCell {
 // MARK: - Lifecycle 🌎
 extension ElongationCell {
   
+  open override func willMove(toSuperview newSuperview: UIView?) {
+    super.willMove(toSuperview: newSuperview)
+    
+    topViewHeightConstraint?.constant = appearance.topViewHeight
+    guard appearance.parallaxEnabled else { return }
+    scalableViewBottomConstraint?.constant -= 2 * appearance.parallaxFactor
+    scalableViewTopOffset = scalableViewTopConstraint?.constant ?? 0
+    scalableViewBottomOffset = scalableViewBottomConstraint?.constant ?? 0
+  }
+  
   open override func layoutSubviews() {
     super.layoutSubviews()
     if #available(iOS 10, *) {
@@ -80,8 +96,36 @@ extension ElongationCell {
   }
   
   open override func updateConstraints() {
-    frontViewHeightConstraint?.constant = appearance.frontViewHeight
+    
     super.updateConstraints()
+  }
+  
+}
+
+// MARK: - Setup ⛏
+private extension ElongationCell {
+  
+  func configureCell() {
+    selectionStyle = .none
+    selectedBackgroundView = nil
+    clipsToBounds = true
+    contentView.clipsToBounds = true
+  }
+  
+  func addDimmingView() {
+    dimmingView = UIView()
+    contentView.addSubview(dimmingView)
+    dimmingView.alpha = 0
+    dimmingView.backgroundColor = UIColor.black
+    dimmingView.frame = bounds
+    dimmingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+  }
+  
+  func addCustomSeparatorIfNeeded() {
+    guard appearance.customSeparatorEnabled else { return }
+    let color = appearance.separatorColor
+    let topSeparator = UIView()
+    topSeparator.backgroundColor = color
   }
   
 }
@@ -94,9 +138,9 @@ extension ElongationCell {
     isExpanded = value
     
     if animated {
-      UIView.animate(withDuration: 0.3) {
+      UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut, animations: {
         self.updateCellState()
-      }
+      }, completion: nil)
     } else {
       updateCellState()
     }
@@ -104,7 +148,12 @@ extension ElongationCell {
   
   open func dim(_ value: Bool, animated: Bool = true) {
     let alpha: CGFloat = value ? 0.9 : 0
-    UIView.animate(withDuration: 0.2) {
+    if animated {
+      UIView.animate(withDuration: 0.2) {
+        self.dimmingView.alpha = alpha
+        self.contentView.backgroundColor = value ? .black : .white
+      }
+    } else {
       self.dimmingView.alpha = alpha
       self.contentView.backgroundColor = value ? .black : .white
     }
@@ -113,18 +162,112 @@ extension ElongationCell {
   // MARK: Private
   fileprivate func updateCellState() {
     let backColor: UIColor = isExpanded ? .black : .white
-    self.backgroundColor = backColor
-    self.contentView.backgroundColor = backColor
+    backgroundColor = backColor
+    contentView.backgroundColor = backColor
     
-    let frontViewHeight = self.appearance.frontViewHeight
-    self.backViewTopConstraint.constant = isExpanded ? frontViewHeight - self.appearance.backViewOffset : self.appearance.backViewOffset
-    self.backViewHeightConstraint.constant = isExpanded ? self.appearance.backViewHeight : frontViewHeight - self.appearance.backViewOffset
+    if appearance.parallaxEnabled {
+      let value = scalableViewLastParrallaxOffset ?? 0
+      scalableViewTopConstraint.constant = isExpanded ? 0 : -value
+      scalableViewBottomConstraint.constant = isExpanded ? 0 : value
+      print(value)
+    }
     
-    let frontViewScale = isExpanded ? self.appearance.frontViewScaleFactor : 1
-    self.frontView.transform = CGAffineTransform(scaleX: frontViewScale, y: frontViewScale)
+    let frontViewHeight = self.appearance.topViewHeight
+    bottomViewTopConstraint.constant = isExpanded ? frontViewHeight - appearance.bottomViewOffset : appearance.bottomViewOffset
+    bottomViewHeightConstraint.constant = isExpanded ? appearance.bottomViewHeight : frontViewHeight - appearance.bottomViewOffset
     
-    self.contentView.setNeedsLayout()
-    self.contentView.layoutIfNeeded()
+    let frontViewScale = appearance.scaleViewScaleFactor
+    scalableView.transform = isExpanded ? CGAffineTransform(scaleX: frontViewScale, y: frontViewScale) : .identity
+    
+    contentView.setNeedsLayout()
+    contentView.layoutIfNeeded()
+  }
+  
+  func setFrontViewOffset(_ offset: CGFloat) {
+    guard appearance.parallaxEnabled, let topConstraint = scalableViewTopConstraint, let bottomConstraint = scalableViewBottomConstraint else { return }
+    let boundOffset = max(0, min(1, offset))
+    let pixelOffset = (1 - boundOffset) * 2 * appearance.parallaxFactor
+    
+    topConstraint.constant = scalableViewTopOffset - pixelOffset
+    bottomConstraint.constant = scalableViewBottomOffset + pixelOffset
+    scalableViewLastParrallaxOffset = pixelOffset
+  }
+  
+}
+
+// MARK: - Endode/Decode
+extension ElongationCell {
+  
+  private struct Keys {
+    static let isExpanded = "isExpanded"
+    static let frontViewHeightConstraint = "frontViewHeightConstraint"
+    static let dimmingView = "dimmingView"
+    
+    static let scalableView = "scalableView"
+    
+    static let topView = "topView"
+    static let topViewHeightConstraint = "topViewHeightConstraint"
+    
+    static let bottomView = "backView"
+    static let bottomViewHeightConstraint = "bottomViewHeightConstraint"
+    static let bottomViewTopConstraint = "bottomViewTopConstraint"
+    static let bottomViewBottomConstraint = "bottomViewBottomConstraint"
+  }
+  
+  var cellCopy: ElongationCell? {
+    let data = NSKeyedArchiver.archivedData(withRootObject: self)
+    guard case let copy as ElongationCell = NSKeyedUnarchiver.unarchiveObject(with: data) else {
+      return nil
+    }
+    return copy
+  }
+  
+  open override func encode(with aCoder: NSCoder) {
+    super.encode(with: aCoder)
+    aCoder.encode(isExpanded, forKey: Keys.isExpanded)
+    aCoder.encode(topView, forKey: Keys.topView)
+    aCoder.encode(scalableView, forKey: Keys.scalableView)
+    aCoder.encode(topViewHeightConstraint, forKey: Keys.topViewHeightConstraint)
+    aCoder.encode(bottomView, forKey: Keys.bottomView)
+    aCoder.encode(bottomViewTopConstraint, forKey: Keys.bottomViewTopConstraint)
+    aCoder.encode(bottomViewHeightConstraint, forKey: Keys.bottomViewHeightConstraint)
+    aCoder.encode(bottomViewBottomConstraint, forKey: Keys.bottomViewBottomConstraint)
+  }
+  
+  fileprivate func decode(from coder: NSCoder) {
+    
+    if let isExpanded = coder.decodeObject(forKey: Keys.isExpanded) as? Bool {
+      self.isExpanded = isExpanded
+    }
+    
+    if let topView = coder.decodeObject(forKey: Keys.topView) as? UIView {
+      self.topView = topView
+    }
+    
+    if let scalableView = coder.decodeObject(forKey: Keys.scalableView) as? UIView {
+      self.scalableView = scalableView
+    }
+    
+    if let topViewHeightConstraint = coder.decodeObject(forKey: Keys.topViewHeightConstraint) as? NSLayoutConstraint {
+      self.topViewHeightConstraint = topViewHeightConstraint
+    }
+    
+    if let backView = coder.decodeObject(forKey: Keys.bottomView) as? UIView {
+      self.bottomView = backView
+    }
+    
+    if let bottomViewTopConstraint = coder.decodeObject(forKey: Keys.bottomViewTopConstraint) as? NSLayoutConstraint {
+      self.bottomViewTopConstraint = bottomViewTopConstraint
+    }
+    
+    if let bottomViewHeightConstraint = coder.decodeObject(forKey: Keys.bottomViewHeightConstraint) as? NSLayoutConstraint {
+      self.bottomViewHeightConstraint = bottomViewHeightConstraint
+    }
+    
+    if let bottomViewBottomConstraint = coder.decodeObject(forKey: Keys.bottomViewBottomConstraint) as? NSLayoutConstraint {
+      self.bottomViewBottomConstraint = bottomViewBottomConstraint
+    }
+    
   }
   
 }
