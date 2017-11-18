888e482b9bbfc2633a091e72736a526877de3d45
Abdurahim Jauzee <jauzee@mail.ru>
2017-11-18 05:31:11 +0300

    Add support for UITabBarController
    Should fix #9

--- ElongationPreview/Source/ElongationTransition.swift
+++ ElongationPreview/Source/ElongationTransition.swift
@@ -44,6 +44,8 @@ public class ElongationTransition: NSObject {
       for case let elongationViewController as ElongationViewController in navi.viewControllers {
         return elongationViewController
       }
+    } else if let tab = viewController as? UITabBarController, let elongationViewController = tab.selectedViewController as? ElongationViewController {
+      return elongationViewController
     } else if let elongationViewController = viewController as? ElongationViewController {
       return elongationViewController
     }
