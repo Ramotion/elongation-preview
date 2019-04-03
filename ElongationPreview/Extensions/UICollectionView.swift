cecbb1f8d85bb830399afa9973c1eb40c9306998
Alex K <kalinkin.aleksey@gmail.com>
2019-04-03 15:13:58 +0300

    converted to swift 5

--- ElongationPreview/Extensions/UICollectionView.swift
+++ ElongationPreview/Extensions/UICollectionView.swift
@@ -13,19 +13,19 @@ public extension UICollectionView {
 
     /// Register given `UICollectionViewCell` in collectionView.
     /// Cell will be registered with the name of it's class as identifier.
-    public func register<T: UICollectionViewCell>(_: T.Type) {
+    func register<T: UICollectionViewCell>(_: T.Type) {
         register(T.self, forCellWithReuseIdentifier: String(describing: T.self))
     }
 
     /// Register `UICollectionViewCell` from given nib in collectionView.
     /// Cell will be registered with the name of it's class as identifier.
-    public func registerNib<T: UICollectionViewCell>(_: T.Type) {
+    func registerNib<T: UICollectionViewCell>(_: T.Type) {
         let nib = UINib(nibName: String(describing: T.self), bundle: nil)
         register(nib, forCellWithReuseIdentifier: String(describing: T.self))
     }
 
     /// Dequeue cell of given class from tableView.
-    public func dequeue<T: UICollectionViewCell>(_: T.Type, indexPath: IndexPath) -> T {
+    func dequeue<T: UICollectionViewCell>(_: T.Type, indexPath: IndexPath) -> T {
         return dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as! T
     }
 }
--- ElongationPreview/Extensions/UIEdgeInsets.swift
+++ ElongationPreview/Extensions/UIEdgeInsets.swift
@@ -13,7 +13,7 @@ public extension UIEdgeInsets {
 
     /// Initialize UIEdgeInsets with given value for all the sides.
     /// UIEdgeInsets(15) == UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
-    public init(_ padding: CGFloat) {
+    init(_ padding: CGFloat) {
         self.init()
         top = CGFloat(padding)
         bottom = CGFloat(padding)
@@ -23,7 +23,7 @@ public extension UIEdgeInsets {
 
     /// Initialize UIEdgeInsets with given values for top & bottom, left & right sides.
     /// UIEdgeInsets(padding: 15, sidePadding: 16) == UIEdgeInsets(top: 15, left: 16, bottom: 15, right: 16)
-    public init(padding: CGFloat, sidePadding: CGFloat = 0) {
+    init(padding: CGFloat, sidePadding: CGFloat = 0) {
         self.init()
         top = padding; bottom = padding
         left = sidePadding; right = sidePadding
@@ -32,7 +32,7 @@ public extension UIEdgeInsets {
     /// Initialize UIEdgeInsets with inset on desired side.
     /// UIEdgeInsets(top: 20) == UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
     /// UIEdgeInsets(top: 20, otherSides: 15) == UIEdgeInsets(top: 20, left: 15, bottom: 15, right: 15)
-    public init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil, otherSides: CGFloat? = nil) {
+    init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil, otherSides: CGFloat? = nil) {
         self.init()
         self.top = top ?? otherSides ?? 0
         self.left = left ?? otherSides ?? 0
